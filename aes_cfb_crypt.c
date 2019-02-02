45a6d06371d003321bd202da1b29d2a0eb98cfba
sstutanotam <40928227+sstutanotam@users.noreply.github.com>
2019-02-02 14:39:56 +0800

    Update aes_cfb_crypt.c

--- aes_cfb_crypt.c
+++ aes_cfb_crypt.c
@@ -2,24 +2,10 @@
 #include <string.h>
 #include <time.h>
 
-//#include <stdio.h>
-
-// Enable ECB, CTR and CBC mode. Note this can be done before including aes.h or at compile-time.
-// E.g. with GCC by using the -D flag: gcc -c aes.c -DCBC=0 -DCTR=1 -DECB=1
-//#define CBC 1
-//#define CTR 1
-//#define ECB 1
-//#define CFB 1
-//#define AES256 1
-//#define AES128 1
-
-#include "aes.h"
 #include "modes.h"
 #include "sha.h"
+#include "aes_cfb_crypt.h"
 
-
-int sha384_key_iv(char * in, size_t length, unsigned char * key, unsigned char * iv);
-int aes_256_cfb_crypt(const unsigned char *in, unsigned char *out, size_t length, char *strPwd, unsigned char *crypt_iv, int mode);
 
 void AES_cfb128_encrypt(const unsigned char *in, unsigned char *out,
                         size_t length, const AES_KEY *key,
@@ -35,131 +21,117 @@ void AES_cfb128_encrypt(const unsigned char *in, unsigned char *out,
 int randomIV(unsigned char *out)
 {
 	srand(time(NULL));
-	int t_enc_len = 32*sizeof(char);
-	char *t_enc = (char *)malloc(t_enc_len);
+	char *t_enc = (char *)malloc(SHA512_DIGEST_LENGTH);
 	int iLen = sizeof(int);
-	int _iLen = iLen;
+	int loop = SHA512_DIGEST_LENGTH / iLen;
 	int t;
-	while(_iLen <= t_enc_len)
-	{
+	while(loop--) {
 		t = rand();
-		memcpy(t_enc + _iLen - iLen, (char *)&t, iLen);
-		_iLen += iLen;
+		memcpy(t_enc + loop * iLen, (char *)&t, iLen);
+	}
+	t = sizeof(SHA512_CTX);
+	SHA512_CTX *ctx = (SHA512_CTX *)malloc(t);
+	SHA512_Init(ctx);
+	unsigned char *digest = malloc(SHA512_DIGEST_LENGTH);
+	SHA512_Update(ctx, t_enc, SHA512_DIGEST_LENGTH);
+	SHA512_Final(digest, ctx);
+	if(AES_IV_SIZE > SHA512_DIGEST_LENGTH) {
+		memcpy(out, digest, SHA512_DIGEST_LENGTH);
+	} else {
+		memcpy(out, digest + (rand() % (SHA512_DIGEST_LENGTH - AES_IV_SIZE + 1)), AES_IV_SIZE);
 	}
 
-	SHA512_CTX ctx;
-	SHA384_Init(&ctx);
-	int digest_len = SHA384_DIGEST_LENGTH*sizeof(char);
-	unsigned char *digest = malloc(digest_len);
-	SHA384_Update(&ctx, t_enc, t_enc_len);
-	SHA384_Final(digest, &ctx);
-	t = rand() % (digest_len - 16 + 1);
-	memcpy(out, digest + t, 16);
-
-	OPENSSL_cleanse(&ctx, sizeof(ctx));
-	OPENSSL_cleanse(digest, digest_len);
+	OPENSSL_cleanse(ctx, t);
+	OPENSSL_cleanse(digest, SHA512_DIGEST_LENGTH);
+	free(ctx);
 	free(digest);
+	memset(t_enc, 0, SHA512_DIGEST_LENGTH);
 	free(t_enc);
-	return 16;
+	return AES_IV_SIZE;
 }
 
 int randomIv(unsigned char *out)
 {
 	srand(time(NULL));
-	int t_enc_len = 32*sizeof(char);
+	int t_enc_len = 2 * AES_BLOCK_SIZE * sizeof(char);
 	char *t_enc = (char *)malloc(t_enc_len);
 	int iLen = sizeof(int);
-	int _iLen = iLen;
-	while(_iLen <= t_enc_len)
-	{
-		int t = rand();
-		/*printf("%s\n", (char *)&t);*/
-		memcpy(t_enc + _iLen - iLen, (char *)&t, iLen);
-		_iLen += iLen;
+	int loop = t_enc_len / iLen;
+	int t;
+	while(loop--) {
+		t = rand();
+		memcpy(t_enc + loop * iLen, (char *)&t, iLen);
 	}
-	/*
-	printf("t_enc = %s\n", t_enc);
-	printf("t_enc = %s\n", t_enc + iLen);
-	printf("t_enc = %s\n", t_enc + iLen*2);
-	printf("t_enc = %s\n", t_enc + iLen*3);
-	printf("t_enc = %s\n", t_enc + iLen*4);
-	printf("t_enc = %s\n", t_enc + iLen*5);
-	printf("t_enc = %s\n", t_enc + iLen*6);
-	printf("t_enc = %s\n", t_enc + iLen*7);
-	*/
-
 	AES_KEY aesKey;
-	int setKey = AES_set_encrypt_key((const unsigned char *)t_enc, 128, &aesKey);
-	if(setKey == 0)
-	{
-		AES_encrypt((const unsigned char *)t_enc + 16, out, &aesKey);
+	t = AES_set_encrypt_key((const unsigned char *)t_enc, 128, &aesKey);
+	if(t == 0) {
+		AES_encrypt((const unsigned char *)t_enc + AES_BLOCK_SIZE, out, &aesKey);
+		memset(t_enc, 0, t_enc_len);
 		free(t_enc);
-		return 16;
-	}
-	else
-	{
+		return AES_BLOCK_SIZE;
+	} else {
+		memset(t_enc, 0, t_enc_len);
 		free(t_enc);
 		return 0;
 	}
 }
 
-int aes_256_cfb_crypt(const unsigned char *in, unsigned char *out, size_t length, char *strPwd, unsigned char *crypt_iv, int mode)
+int random_iv(unsigned char *out)
+{
+	srand(time(NULL));
+	int _len = sizeof(int) / 2;
+	int loop = AES_BLOCK_SIZE * sizeof(char) / _len;
+	int t;
+	while(loop--) {
+		t = rand();
+		/*memcpy(out + loop * _len, (unsigned char *)&t, _len);*/
+		memcpy(out + loop * _len, (char *)&t, _len);
+	}
+	t = 0;
+	return AES_BLOCK_SIZE;
+}
+
+int aes_256_cfb_crypt(const unsigned char *in, unsigned char *out, size_t length, char *strPwd, unsigned char *crypt_iv, int *num, int mode)
 {
 	unsigned char *key = (unsigned char *)malloc(32);
-	unsigned char *iv = (unsigned char *)malloc(16);
+	unsigned char *iv = (unsigned char *)malloc(AES_IV_SIZE);
 	sha384_key_iv(strPwd, strlen(strPwd), key, iv);
 	AES_KEY aesKey;
 	int setKey = AES_set_encrypt_key((const unsigned char *)key, 256, &aesKey);
 	free(key);
-	if (setKey != 0)
-	{
+	if (setKey != 0) {
 		return -1;
 	}
-	if(mode == 1)
-	{
-		int num = 0;
-		AES_cfb128_encrypt(crypt_iv, out, 16, (const void *)&aesKey, iv, &num, AES_ENCRYPT);
-		if(num != 0)
-		{
+	if(mode == 1) {
+		AES_cfb128_encrypt(crypt_iv, out, AES_IV_SIZE, (const void *)&aesKey, iv, num, AES_ENCRYPT);
+		if(*num != 0) {
 			return -2;
 		}
-		num = 0;
-		AES_cfb128_encrypt(in, out + 16, length, (const void *)&aesKey, crypt_iv, &num, AES_ENCRYPT);
+		AES_cfb128_encrypt(in, out + AES_IV_SIZE, length, (const void *)&aesKey, crypt_iv, num, AES_ENCRYPT);
 		free(iv);
 		/*
-		if(num == (length % 16))
+		if(num == (length % AES_BLOCK_SIZE))
 		{
 			printf("good~\n");
 		}
 		*/
-		return num;
-	}
-	else if(mode == 2)
-	{
-		int num = 0;
-		AES_cfb128_encrypt(in, crypt_iv, 16, (const void *)&aesKey, iv, &num, AES_DECRYPT);
-		if(num != 0)
-		{
+		return length + AES_IV_SIZE;
+	} else if(mode == 2) {
+		AES_cfb128_encrypt(in, crypt_iv, AES_IV_SIZE, (const void *)&aesKey, iv, num, AES_DECRYPT);
+		if(*num != 0) {
 			return -2;
 		}
-		num = 0;
-		AES_cfb128_encrypt(in + 16, out, length - 16, (const void *)&aesKey, crypt_iv, &num, AES_DECRYPT);
+		AES_cfb128_encrypt(in + AES_IV_SIZE, out, length - AES_IV_SIZE, (const void *)&aesKey, crypt_iv, num, AES_DECRYPT);
 		free(iv);
-		return num;
-	}
-	else if(mode == 3)
-	{
-		int num = 0;
-		AES_cfb128_encrypt(in, out, length, (const void *)&aesKey, crypt_iv, &num, AES_ENCRYPT);
+		return length - AES_IV_SIZE;
+	} else if(mode == 3) {
+		AES_cfb128_encrypt(in, out, length, (const void *)&aesKey, crypt_iv, num, AES_ENCRYPT);
 		free(iv);
-		return num;
-	}
-	else if(mode == 4)
-	{
-		int num = 0;
-		AES_cfb128_encrypt(in, out, length, (const void *)&aesKey, crypt_iv, &num, AES_DECRYPT);
+		return length;
+	} else if(mode == 4) {
+		AES_cfb128_encrypt(in, out, length, (const void *)&aesKey, crypt_iv, num, AES_DECRYPT);
 		free(iv);
-		return num;
+		return length;
 	}
 	return -3;
 }
@@ -167,20 +139,20 @@ int aes_256_cfb_crypt(const unsigned char *in, unsigned char *out, size_t length
 
 int sha384_key_iv(char * in, size_t length, unsigned char * key, unsigned char * iv)
 {
-	SHA512_CTX ctx;
-	SHA384_Init(&ctx);
-	/*char digest[SHA384_DIGEST_LENGTH] = {0};*/
-	int digest_len = SHA384_DIGEST_LENGTH*sizeof(char);
-	unsigned char *digest = malloc(digest_len);
-	SHA384_Update(&ctx, in, length);
-	SHA384_Final(digest, &ctx);
+	int ctxLen = sizeof(SHA512_CTX);
+	SHA512_CTX *ctx = (SHA512_CTX *)malloc(ctxLen);
+	SHA384_Init(ctx);
+	unsigned char *digest = (unsigned char *)malloc(SHA384_DIGEST_LENGTH);
+	SHA384_Update(ctx, in, length);
+	SHA384_Final(digest, ctx);
 	memcpy(key, digest, 32);
 	/*key[32] = '\0';*/
-	memcpy(iv, &digest[32], 16);
-	/*iv[16] = '\0';*/
+	memcpy(iv, digest + 32, SHA384_DIGEST_LENGTH - 32);
+	/*iv[AES_IV_SIZE] = '\0';*/
 
-	OPENSSL_cleanse(&ctx, sizeof(ctx));
-	OPENSSL_cleanse(digest, digest_len);
+	OPENSSL_cleanse(ctx, ctxLen);
+	OPENSSL_cleanse(digest, SHA384_DIGEST_LENGTH);
+	free(ctx);
 	free(digest);
 	return 32;
 }
